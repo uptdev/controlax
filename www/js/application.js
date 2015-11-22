fa6dd9d2fe0a7a723d515d20ee45da3ec16fd7c3
lapisdecor <lapisdecor@gmail.com>
2015-11-22 12:38:31 +0000

    add comments and better control over input values

--- www/js/application.js
+++ www/js/application.js
@@ -14,12 +14,30 @@ Application.prototype.initialized = function() {
 };
 
 document.getElementById("calcular").addEventListener('click', function(){
+
+    // vai buscar os valores do peso e da altura aos textfields através do Id
     var peso = document.getElementById("peso").value;
     var altura = document.getElementById("altura").value;
-    if (!isNaN(peso) && !isNaN(altura)){
+
+    // converte o peso e a altura em numeros
+    peso = Number(peso);
+    altura = Number(altura);
+
+    if (!isNaN(peso) && !isNaN(altura) && (peso != 0 && altura != 0)) {
+
+        // calcula imc
         var imc = peso/(altura * altura);
+
+        // mostra apenas 1 casa decimal
         imc = imc.toFixed(1);
-        document.getElementById("result").innerHTML = "O seu IMC é " + imc;
+
+        // escreve no documento
+        write = document.getElementById("result")
+        write.innerHTML = "O seu IMC é " + imc + "<p></p>";
+
+    }
+    else {
+        document.getElementById("result").innerHTML = "Tem de introduzir o peso e a altura."
     }
     });
 
